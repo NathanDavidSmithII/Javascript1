67d93a5017e422718d966b7fdb43af0ee4d6cad7
David Smith <redjunkopera@gmail.com>
2021-09-13 17:31:51 -0400

    first commit

---
+++ lesson1/script.js
@@ -0,0 +1,7 @@
+window.onload = function WindowLoad(event) {
+alert('Happy Wednesday Evening!');
+let variableName = prompt('Please enter your name!');
+let greeting= variableName + ', thank you so much for coming to my website.';
+
+document.getElementById("output").innerHTML = greeting; }
+

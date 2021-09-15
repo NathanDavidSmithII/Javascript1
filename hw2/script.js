db24a5b9ebb63d7e2fa5476d7ba043affb320d40
David Smith <redjunkopera@gmail.com>
2021-09-15 19:04:26 -0400

    hw2

--- hw2/script.js
+++ hw2/script.js
@@ -1,5 +1,25 @@
 window.onload = function WindowLoad(event) {
-var motto = prompt ("Please enter your favourite quote!")
+let variableName = prompt ("Please enter your favourite motto!");
+let greeting = variableName + ", is a great motto, thanks for sharing!";
+document.getElementById("output").innerHTML = greeting; 
 
+/*function repeatgreeting(string, times) {
+    var greeting = "";
+    while (times > 0) {
+      greeting += string;
+      times--;
+    }
+    return greeting;
+  }*/
+  /*let display = "";
+  let i = 1;
+  while (i<=100)
+  { display += i + "<br>";
+      i++;
+  }    
+  document.getElementById("while-output").innerHTML="";
+  let display = "";
+  for (let i = 1; i <= 100; i++){display += i +"<br>";}
+  document.getElementById("for-output").innerHTML="";*/
 
 }

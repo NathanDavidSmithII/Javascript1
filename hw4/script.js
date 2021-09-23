075fdd3316499ce680a5d75a05b924b79d1d73ed
David Smith <redjunkopera@gmail.com>
2021-09-22 20:07:49 -0400

    hw4

---
+++ hw4/script.js
@@ -0,0 +1,21 @@
+window.onload = function WindowLoad(event) {
+   
+    const randomNumber = Math.floor(Math.random() * 100) + 1
+    console.log('Random Number', randomNumber)
+
+    function checkGuess() {
+      let myGuess = guess.value
+      if (myGuess === randomNumber) {
+        feedback.textContent = "You got it right!"
+      } else if (myGuess > randomNumber) {
+        feedback.textContent = "Your guess was " + myGuess + ". That's too high. Try Again!"
+      } else if (myGuess < randomNumber) {
+       feedback.textContent = "Your guess was " + myGuess + ". That's too low. Try Again!"
+     }
+     document.getElementById("feedback"); 
+    }
+   submitGuess.addEventListener('click', checkGuess)
+
+
+
+}
